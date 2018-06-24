71baa9060d67425bc0c9e4790f0c32d9f7de5199
Atsushi <atsushimtzk3@gmail.com>
2018-06-24 01:33:00 +0000

    test commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:notice] = 'Sign up 成功！　Log in してください'
       redirect_to user_path(@user.id)
     else
       render 'new'
